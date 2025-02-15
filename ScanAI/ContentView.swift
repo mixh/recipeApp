1ccbbe88c7451479b75ae7ce7061f09787045732
Mihir Jain <mihirxjain@gmail.com>
2025-02-15 16:53:59 -0500

    Initial Commit

---
+++ ScanAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScanAI
+//
+//  Created by Mihir Jain on 2/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScanAI/ScanAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanAIApp.swift
+//  ScanAI
+//
+//  Created by Mihir Jain on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ScanAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
